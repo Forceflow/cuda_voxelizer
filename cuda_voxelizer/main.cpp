fbe1c71bb359750119bbefde9a0d5fd977368fd1
Jeroen Baert <jerbae@gmail.com>
2014-04-29 00:04:01 +0200

    Added function to allocate host memory for triangles

--- cuda_voxelizer/main.cpp
+++ cuda_voxelizer/main.cpp
@@ -1,6 +1,58 @@
-#include "TriMesh.h"
 #include "voxelize.cuh"
+#include "TriMesh.h"
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+string filename = "";
+int gridsize = 1024;
+float* triangles;
+
+void parseProgramParameters(int argc, char* argv[]){
+	if(argc<2){ // not enough arguments
+		exit(0);
+	} 
+	for (int i = 1; i < argc; i++) {
+		if (string(argv[i]) == "-f") {
+			filename = argv[i + 1]; 
+			i++;
+		} else if (string(argv[i]) == "-s") {
+			gridsize = atoi(argv[i + 1]);
+			i++;
+		}
+	}
+	fprintf(stdout, "Filename: %s \n", filename.c_str());
+	fprintf(stdout, "Grid size: %i \n", gridsize);
+}
+
+// Helper function to transfer triangles to CUDA-allocated pinned host memory
+inline void trianglesToMemory(const trimesh::TriMesh *mesh, float** _triangles){
+	// allocate pinned host memory
+	//HANDLE_CUDA_ERROR(cudaHostAlloc(_triangles,sizeof(float)*9*mesh->faces.size(),cudaHostAllocDefault));
+	// loop over all triangles and place them in memory
+	trimesh::point v0,v1,v2;
+	for(size_t i = 0; i < mesh->faces.size(); i++){
+		v0 = mesh->vertices[mesh->faces[i][0]];
+		v1 = mesh->vertices[mesh->faces[i][1]];
+		v2 = mesh->vertices[mesh->faces[i][2]];
+	}
+}
+
 
 int main(int argc, char *argv[]) {
+	parseProgramParameters(argc, argv);
 	checkCudaRequirements();
+
+	allocateHostMem(80, (void**) &triangles);
+
+	trimesh::TriMesh *themesh = trimesh::TriMesh::read(filename.c_str());
+	themesh->need_faces(); // unpack (possible) triangle strips so we have faces
+	themesh->need_bbox(); // compute the bounding box
+
+	fprintf(stdout, "Number of faces: %ull, faces table takes %ull bytes \n", themesh->faces.size(), themesh->faces.size()*sizeof(trimesh::TriMesh::Face));
+	fprintf(stdout, "Number of vertices: %ull, vertices table takes %ull bytes \n", themesh->vertices.size(), themesh->vertices.size()*sizeof(trimesh::point));
+	fprintf(stdout, "Flat triangle table will take %ull bytes \n", themesh->faces.size()*9*sizeof(float));
+
+	
+
 }
--- cuda_voxelizer/voxelize.cuh
+++ cuda_voxelizer/voxelize.cuh
@@ -1,22 +1,33 @@
 #ifndef VOXELIZE_H_
 #define VOXELIZE_H_
 
+#include <stdio.h>
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "cuda_error_check.cuh"
-#include <stdio.h>
+
+inline void allocateHostMem(size_t size, void **data){
+	HANDLE_CUDA_ERROR(cudaHostAlloc(data, size, cudaHostAllocDefault));
+}
 
 // Check if CUDA requirements are met
 inline int checkCudaRequirements(){
+	// Is there a cuda device?
 	int device_count = 0;
 	HANDLE_CUDA_ERROR(cudaGetDeviceCount(&device_count));
 	if(device_count < 1){
-		fprintf(stderr, "No cuda devices found - we need at least one");
+		fprintf(stderr, "No cuda devices found - we need at least one \n");
 		return 0;
 	} else {
-		fprintf(stdout, "Found %i cuda devices, yay!", device_count);
+		fprintf(stdout, "Found %i cuda devices, yay! \n", device_count);
 	}
-	CHECK_CUDA_ERROR();
+
+	// We'll be using first device by default
+	cudaDeviceProp properties;
+	HANDLE_CUDA_ERROR(cudaSetDevice(0));
+	HANDLE_CUDA_ERROR(cudaGetDeviceProperties(&properties,0));
+	fprintf(stdout,"Device %d: \"%s\"\n", 0, properties.name);
+	fprintf(stdout,"Available global device memory: %llu bytes", properties.totalGlobalMem);
 	return 1;
 }
 
