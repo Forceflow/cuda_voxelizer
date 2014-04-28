9c1e7295dedeadefa5d228ff382780aa1f25299e
Jeroen Baert <jerbae@gmail.com>
2014-04-29 00:03:43 +0200

    Added cuda constants

--- cuda_voxelizer/voxelize.cu
+++ cuda_voxelizer/voxelize.cu
@@ -1,7 +1,13 @@
 #include "voxelize.cuh"
 
+// symbols for constant memory: the same for every triangle
+__constant__ float3 model_bbox_min;
+__constant__ float3 model_bbox_max;
+__constant__ float unitlength;
+
 __global__ void kernel()
 {
+	
 }
 
 // Helper function for using CUDA to add vectors in parallel.
@@ -9,4 +15,5 @@ cudaError_t voxelize()
 {
     cudaError_t cudaStatus = cudaSuccess;
     return cudaStatus;
+	
 }
