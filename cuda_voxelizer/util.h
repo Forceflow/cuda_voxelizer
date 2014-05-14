3748574edd31dcebb89451d098d10ee248263199
jerbae@gmail.com <jerbae@gmail.com>
2014-05-14 18:41:31 +0200

    Added util.h for general structs

---
+++ cuda_voxelizer/util.h
@@ -0,0 +1,10 @@
+#include <glm/glm.hpp>
+
+// voxelisation info (same for every triangle)
+struct voxinfo {
+	size_t n_triangles;
+	unsigned int gridsize;
+	float unitlength;
+	glm::vec3 bbox_min;
+	glm::vec3 bbox_max;
+};
