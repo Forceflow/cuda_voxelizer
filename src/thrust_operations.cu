0a4c349aa3bc90a1d88e4bedc2fe4e6ae2cdc1ea
Jeroen Baert <jerbae@gmail.com>
2019-02-17 17:31:59 +0100

    Force CUDA_GLM_PURE

--- src/thrust_operations.cu
+++ src/thrust_operations.cu
@@ -1,12 +1,12 @@
 #include "thrust_operations.cuh"
 
-// Thrust vectors (global)
+// thrust vectors (global)
 thrust::host_vector<glm::vec3> trianglethrust_host;
 thrust::device_vector<glm::vec3> trianglethrust_device;
 
-// METHOD 3: Use a thrust vector
+// method 3: use a thrust vector
 void trianglesToGPU_thrust(const trimesh::TriMesh *mesh, float** triangles) {
-	// Fill host vector
+	// fill host vector
 	for (size_t i = 0; i < mesh->faces.size(); i++) {
 		glm::vec3 v0 = trimesh_to_glm<trimesh::point>(mesh->vertices[mesh->faces[i][0]]);
 		glm::vec3 v1 = trimesh_to_glm<trimesh::point>(mesh->vertices[mesh->faces[i][1]]);
--- src/thrust_operations.cuh
+++ src/thrust_operations.cuh
@@ -9,6 +9,7 @@
 #include "thrust/device_vector.h"
 #include "thrust/host_vector.h"
 #define GLM_FORCE_CUDA
+#define GLM_FORCE_PURE
 // GLM for maths
 #include <glm/glm.hpp>
 #include "util.h"
