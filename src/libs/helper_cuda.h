608f7256a1c93f645a6bc249b9f16dc106111189
Jeroen Baert <jerbae@gmail.com>
2019-02-19 13:05:42 +0100

    Fix extra whiteline on output

--- src/libs/helper_cuda.h
+++ src/libs/helper_cuda.h
@@ -779,7 +779,7 @@ inline int findCudaDevice(int argc, const char **argv) {
     devID = gpuGetMaxGflopsDeviceId();
     checkCudaErrors(cudaSetDevice(devID));
     checkCudaErrors(cudaGetDeviceProperties(&deviceProp, devID));
-    printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID,
+    printf("GPU Device %d: \"%s\" with compute capability %d.%d\n", devID,
            deviceProp.name, deviceProp.major, deviceProp.minor);
   }
 
