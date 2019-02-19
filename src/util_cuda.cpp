854cb35e0ff65cf291e17437381879f6f1babeb0
Jeroen Baert <jerbae@gmail.com>
2019-02-19 13:06:46 +0100

    Select device with maximum FLOPS

--- src/util_cuda.cpp
+++ src/util_cuda.cpp
@@ -2,29 +2,30 @@
 
 // Check if CUDA requirements are met
 int initCuda(){
-	// Is there a cuda device?
+	// Is there a CUDA device?
 	int device_count = 0;
 	checkCudaErrors(cudaGetDeviceCount(&device_count));
 	if(device_count < 1){
-		fprintf(stderr, "No cuda devices found - we need at least one. \n");
-		return 0;
-	} 
+		fprintf(stderr, "[CUDA] No CUDA devices found. \n \n");
+		fprintf(stderr, "[CUDA] Make sure CUDA device is powered, connected and available. \n");
+		fprintf(stderr, "[CUDA] On laptops: disable powersave/battery mode. \n");
+		fprintf(stderr, "[CUDA] Exiting... \n");
+		exit(0);
+	}
 
-	// We'll be using first device by default
+	fprintf(stdout, "[CUDA] ");
+	// Select best (fastest) CUDA device (method from Nvidia helper library)
+	int device = findCudaDevice(0, 0);
+
 	cudaDeviceProp properties;
-	checkCudaErrors(cudaSetDevice(0));
-	checkCudaErrors(cudaGetDeviceProperties(&properties,0));
-	fprintf(stdout,"Device %d: \"%s\".\n", 0, properties.name);
-	fprintf(stdout,"Available global device memory: %llu bytes. \n", properties.totalGlobalMem);
+	checkCudaErrors(cudaGetDeviceProperties(&properties,device));
+	fprintf(stdout,"[CUDA] Available global device memory: %.0lf MB. \n", ((double) properties.totalGlobalMem / 1024 / 1024));
 
 	// Check compute capability
 	if (properties.major < 2){
-		fprintf(stderr, "Your cuda device has compute capability %i.%i. We need at least 2.0 for atomic operations. \n", properties.major, properties.minor);
-		return 0;
-	} else {
-		fprintf(stdout, "Compute capability: %i.%i.\n", properties.major, properties.minor);
+		fprintf(stderr, "[CUDA] Your cuda device has compute capability %i.%i. We need at least 2.0 for atomic operations. Exiting. \n", properties.major, properties.minor);
+		exit(0);
 	}
-
 
 	return 1;
 }
