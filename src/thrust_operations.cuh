4b9a7f0f829775417ffd880ecba9c7e246c8a16a
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2021-05-20 13:38:25 +0200

    Include thrust device/host vector

--- src/thrust_operations.cuh
+++ src/thrust_operations.cuh
@@ -6,8 +6,8 @@
 #include "cuda.h"
 #include "cuda_runtime.h"
 // Thrust
-#include "thrust/device_vector.h"
-#include "thrust/host_vector.h"
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 #define GLM_FORCE_CUDA
 #define GLM_FORCE_PURE
 // GLM for maths
