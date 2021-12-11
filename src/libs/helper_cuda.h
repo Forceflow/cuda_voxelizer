ceaad744196eefba83723ec5fb9ff8174d82d8d7
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2021-12-11 11:36:50 +0100

    Remove newline printing after GPU identify

--- src/libs/helper_cuda.h
+++ src/libs/helper_cuda.h
@@ -869,7 +869,7 @@ inline int findCudaDevice(int argc, const char **argv) {
     int major = 0, minor = 0; 
     checkCudaErrors(cudaDeviceGetAttribute(&major, cudaDevAttrComputeCapabilityMajor, devID));
     checkCudaErrors(cudaDeviceGetAttribute(&minor, cudaDevAttrComputeCapabilityMinor, devID));
-    printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n",
+    printf("GPU Device %d: \"%s\" with compute capability %d.%d\n",
            devID, _ConvertSMVer2ArchName(major, minor), major, minor);
 
   }
