e8775661b5d446803de20772d7e81729715c37b9
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2023-04-02 01:34:16 +0200

    Move away from GLM to cuda types

--- src/voxelize_solid.cu
+++ src/voxelize_solid.cu
@@ -21,11 +21,11 @@ __device__ __inline__ void setBitXor(unsigned int* voxel_table, size_t index) {
 }
 
 //check the location with point and triangle
-__device__ inline int check_point_triangle(glm::vec2 v0, glm::vec2 v1, glm::vec2 v2, glm::vec2 point)
+__device__ inline int check_point_triangle(float2 v0, float2 v1, float2 v2, float2 point)
 {
-	glm::vec2 PA = point - v0;
-	glm::vec2 PB = point - v1;
-	glm::vec2 PC = point - v2;
+	float2 PA = point - v0;
+	float2 PB = point - v1;
+	float2 PC = point - v2;
 
 	float t1 = PA.x*PB.y - PA.y*PB.x;
 	if (fabs(t1) < float_error&&PA.x*PB.x <= 0 && PA.y*PB.y <= 0)
@@ -46,16 +46,16 @@ __device__ inline int check_point_triangle(glm::vec2 v0, glm::vec2 v1, glm::vec2
 }
 
 //find the x coordinate of the voxel
-__device__ inline float get_x_coordinate(glm::vec3 n, glm::vec3 v0, glm::vec2 point)
+__device__ inline float get_x_coordinate(float3 n, float3 v0, float2 point)
 {
 	return (-(n.y*(point.x - v0.y) + n.z*(point.y - v0.z)) / n.x + v0.x);
 }
 
 //check the triangle is counterclockwise or not
-__device__ inline bool checkCCW(glm::vec2 v0, glm::vec2 v1, glm::vec2 v2)
+__device__ inline bool checkCCW(float2 v0, float2 v1, float2 v2)
 {
-	glm::vec2 e0 = v1 - v0;
-	glm::vec2 e1 = v2 - v0;
+	float2 e0 = v1 - v0;
+	float2 e1 = v2 - v0;
 	float result = e0.x*e1.y - e1.x*e0.y;
 	if (result > 0)
 		return true;
@@ -64,7 +64,7 @@ __device__ inline bool checkCCW(glm::vec2 v0, glm::vec2 v1, glm::vec2 v2)
 }
 
 //top-left rule
-__device__ inline bool TopLeftEdge(glm::vec2 v0, glm::vec2 v1)
+__device__ inline bool TopLeftEdge(float2 v0, float2 v1)
 {
 	return ((v1.y<v0.y) || (v1.y == v0.y&&v0.x>v1.x));
 }
@@ -78,46 +78,45 @@ __global__ void voxelize_triangle_solid(voxinfo info, float* triangle_data, unsi
 	while (thread_id < info.n_triangles) { // every thread works on specific triangles in its stride
 		size_t t = thread_id * 9; // triangle contains 9 vertices
 
-								  // COMPUTE COMMON TRIANGLE PROPERTIES
-								  // Move vertices to origin using bbox
-		glm::vec3 v0 = glm::vec3(triangle_data[t], triangle_data[t + 1], triangle_data[t + 2]) - info.bbox.min;
-		glm::vec3 v1 = glm::vec3(triangle_data[t + 3], triangle_data[t + 4], triangle_data[t + 5]) - info.bbox.min;
-		glm::vec3 v2 = glm::vec3(triangle_data[t + 6], triangle_data[t + 7], triangle_data[t + 8]) - info.bbox.min;
+		// COMPUTE COMMON TRIANGLE PROPERTIES
+		// Move vertices to origin using bbox
+		float3 v0 = make_float3(triangle_data[t], triangle_data[t + 1], triangle_data[t + 2]) - info.bbox.min;
+		float3 v1 = make_float3(triangle_data[t + 3], triangle_data[t + 4], triangle_data[t + 5]) - info.bbox.min;
+		float3 v2 = make_float3(triangle_data[t + 6], triangle_data[t + 7], triangle_data[t + 8]) - info.bbox.min;
 		// Edge vectors
-		glm::vec3 e0 = v1 - v0;
-		glm::vec3 e1 = v2 - v1;
-		glm::vec3 e2 = v0 - v2;
+		float3 e0 = v1 - v0;
+		float3 e1 = v2 - v1;
+		float3 e2 = v0 - v2;
 		// Normal vector pointing up from the triangle
-		glm::vec3 n = glm::normalize(glm::cross(e0, e1));
-		if (fabs(n.x) < float_error)
-			return;
+		float3 n = normalize(cross(e0, e1));
+		if (fabs(n.x) < float_error) { return; }
 
 		// Calculate the projection of three point into yoz plane
-		glm::vec2 v0_yz = glm::vec2(v0.y, v0.z);
-		glm::vec2 v1_yz = glm::vec2(v1.y, v1.z);
-		glm::vec2 v2_yz = glm::vec2(v2.y, v2.z);
+		float2 v0_yz = make_float2(v0.y, v0.z);
+		float2 v1_yz = make_float2(v1.y, v1.z);
+		float2 v2_yz = make_float2(v2.y, v2.z);
 
 		// Set the triangle counterclockwise
 		if (!checkCCW(v0_yz, v1_yz, v2_yz))
 		{
-			glm::vec2 v3 = v1_yz;
+			float2 v3 = v1_yz;
 			v1_yz = v2_yz;
 			v2_yz = v3;
 		}
 
 		// COMPUTE TRIANGLE BBOX IN GRID
 		// Triangle bounding box in world coordinates is min(v0,v1,v2) and max(v0,v1,v2)
-		glm::vec2 bbox_max = glm::max(v0_yz, glm::max(v1_yz, v2_yz));
-		glm::vec2 bbox_min = glm::min(v0_yz, glm::min(v1_yz, v2_yz));
+		float2 bbox_max = fmaxf(v0_yz, fmaxf(v1_yz, v2_yz));
+		float2 bbox_min = fminf(v0_yz, fminf(v1_yz, v2_yz));
 
-		glm::vec2 bbox_max_grid = glm::vec2(floor(bbox_max.x / info.unit.y - 0.5), floor(bbox_max.y / info.unit.z - 0.5));
-		glm::vec2 bbox_min_grid = glm::vec2(ceil(bbox_min.x / info.unit.y - 0.5), ceil(bbox_min.y / info.unit.z - 0.5));
+		float2 bbox_max_grid = make_float2(floor(bbox_max.x / info.unit.y - 0.5), floor(bbox_max.y / info.unit.z - 0.5));
+		float2 bbox_min_grid = make_float2(ceil(bbox_min.x / info.unit.y - 0.5), ceil(bbox_min.y / info.unit.z - 0.5));
 
 		for (int y = bbox_min_grid.x; y <= bbox_max_grid.x; y++)
 		{
 			for (int z = bbox_min_grid.y; z <= bbox_max_grid.y; z++)
 			{
-				glm::vec2 point = glm::vec2((y + 0.5)*info.unit.y, (z + 0.5)*info.unit.z);
+				float2 point = make_float2((y + 0.5) * info.unit.y, (z + 0.5) * info.unit.z);
 				int checknum = check_point_triangle(v0_yz, v1_yz, v2_yz, point);
 				if ((checknum == 1 && TopLeftEdge(v0_yz, v1_yz)) || (checknum == 2 && TopLeftEdge(v1_yz, v2_yz)) || (checknum == 3 && TopLeftEdge(v2_yz, v0_yz)) || (checknum == 0))
 				{
